fb278c6fa4d2e470202b5831f0f4f41df8f57f42
lukebacopoulos <baco@bu.edu>
2023-04-30 16:25:20 -0400

    Fixed bug with sourcecode

--- sourcecode/SourceAnalyzer.py
+++ sourcecode/SourceAnalyzer.py
@@ -1,7 +1,7 @@
 import os
 import re
 import yaml
-from typing import Optional
+from typing import Optional, Self
 
 
 class Analyzer:
@@ -25,44 +25,46 @@ class SourceAnalyzer(Analyzer):
                 with open(os.path.join(rules_dir, file_name), 'r') as f:
                     data = yaml.safe_load(f)
                     rules.extend(data['rules'])
+
         return rules
 
 
     def detect(self, path: Optional[str] = None, name: Optional[str] = None,
            version: Optional[str] = None) -> tuple[bool, str]:
-        """
-        Uses a package's source code to determine if the package contains any
-        malware.
-        Args:
-            path (str): The path to the directory containing the package's source code.
-        Returns:
-            bool: True if malware is detected
-            str:  A message describing the malware detected
-        """
         has_issues = False
         messages = []
-        
-        package_info = {"source_code": []}
 
-        # Read source code files from the directory
-        if path is not None and os.path.isdir(path):
+        # Process each file in the source code directory
+        if path is not None and os.path.isdir(path):  # Fix the typo here
             for root, _, files in os.walk(path):
                 for file in files:
                     filepath = os.path.join(root, file)
-                    with open(filepath, "r", encoding="utf-8", errors="ignore") as f:
-                        content = f.readlines()
-                        package_info["source_code"].extend(content)
+                    file_extension = os.path.splitext(file)[-1].lower()
 
-        # Look for patterns in the source code that match the rules defined in the YAML file
-        if "source_code" in package_info:
-            for rule in self.rules:
-                pattern = rule.get('pattern')
-                if pattern is None:
-                    continue
-                for line in package_info["source_code"]:
-                    if re.search(pattern, line):
-                        has_issues = True
-                        messages.append(f"The package's source code matches the {rule['name']} rule: {rule['description']}")
+                    with open(filepath, "r", encoding="utf-8", errors="ignore") as f:
+                        content = f.read()
+
+                    # Check each rule against the current file
+                    for rule in self.rules:
+                        conditions = rule.get('conditions', [])
+
+                        # Check if the rule applies to this file based on conditions
+                        apply_rule = all(
+                            condition.get('file_extension') is None or file_extension in condition['file_extension']
+                            for condition in conditions
+                        )
+
+                        if not apply_rule:
+                            continue
+
+                        # Look for the content pattern in the file content
+                        pattern = rule.get('pattern')
+                        if pattern is not None and re.search(pattern, content):
+                            has_issues = True
+                            messages.append(f"The file {file} matches the {rule['name']} rule: {rule['description']}\n")
 
         return has_issues, "\n".join(messages)
+
+
+
 
